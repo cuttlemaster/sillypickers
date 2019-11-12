6e3291733f8d3c31c611a5e42fdfb475e1bb0060
Caleb Flanagan <caleb.flanagan@clearlink.com>
2019-11-12 13:29:41 -0700

    makes it so you can pick a generator from the list
    Signed-off-by: Caleb Flanagan <caleb.flanagan@clearlink.com>

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,5 @@
+const generators = ["romance-novel-title"];
+
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const months = ["january","february","march","april","may","june","july","august","september","october","november","december"];
 
--- javascript/site.js
+++ javascript/site.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const body = document.querySelector('body');
-
   // STORING ALL THE RELEVANT BUTTONS FOR USE ========== //
   const randomChoiceBtn = document.querySelector('.random-choice');
   const showMeBtn = document.querySelector('.show-me');
@@ -11,18 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputContainer = document.querySelector('.input-container');
   const resultContainer = document.querySelector('.result-container');
 
-  // DEALING WITH THE DIFFERENT BUTTONS GETTING CLICKED ==========/
+  // SELECT A RANDOM GENERATOR FROM THE AVAILABLE LIST WHEN THE
+  // "GIVE ME SOMETHING RANDOM!" BUTTON IS CLICKED BY THE USER
   randomChoiceBtn.addEventListener('click', () => {
-    inputContainer.classList.remove('hide');
-    randomChoiceBtn.classList.add('hide');
+    randomChoiceBtn.classList.add('hide');    // HIDE THE RANDOM CHOICE BUTTON
+    inputContainer.classList.remove('hide');  // SHOW THE DEFAULT CONTAINER FOR ADDING INPUTS
 
-    const generatorChoice = getRandomNumber();
+    // const randomNumber = getRandomNumber();
+    const randomNumber = 0;
+    const chosenGenerator = generators[randomNumber];
+    console.log(chosenGenerator);
   });
 
-  showMeBtn.addEventListener('click', () => {
-    inputContainer.classList.add('hide');
-    resultContainer.classList.remove('hide');
-  });
+  // showMeBtn.addEventListener('click', () => {
+  //   inputContainer.classList.add('hide');
+  //   resultContainer.classList.remove('hide');
+  // });
 
   resetBtn.addEventListener('click', () => {
     resultContainer.classList.add('hide');
