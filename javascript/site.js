b5c40ba2a9e0475da5980f18f6162b0de31e41a4
Caleb Flanagan <caleb.flanagan@clearlink.com>
2019-11-12 17:48:32 -0700

    finalizing the days work
    Signed-off-by: Caleb Flanagan <caleb.flanagan@clearlink.com>

--- javascript/site.js
+++ javascript/site.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // console.log(romanceNovelTitle);
-
   // STORING ALL THE RELEVANT BUTTONS FOR USE ========== //
   const randomChoiceBtn = document.querySelector('.random-choice');
   const pickMyOwnBtn = document.querySelector('.pick-my-own');
   const resetBtn = document.querySelector('.reset');
 
   // STORING THE RELEVANT PAGE CONTAINERS FOR USE ========== //
-  // const inputContainer = document.querySelector('.input-container');
   const resultContainer = document.querySelector('.result-container');
 
   // SELECT A RANDOM GENERATOR FROM THE AVAILABLE LIST WHEN THE
@@ -17,15 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
     randomChoiceBtn.classList.add('hide');    // HIDE THE RANDOM CHOICE BUTTON
     pickMyOwnBtn.classList.add('hide');       // HIDE THE PICK MY OWN BUTTON
 
-    // const randomNumber = getRandomNumber();
     const randomNumber = 0;
     const chosenPicker = availablePickers[randomNumber];
-    //console.log(chosenPicker);
     buildMyPicker(chosenPicker);
   });
 
   pickMyOwnBtn.addEventListener('click', () => {
-    // inputContainer.classList.add('hide');
     resultContainer.classList.remove('hide');
   });
 
