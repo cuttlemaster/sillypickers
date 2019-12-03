8c92cac9c42db32bfbedeb7625c4ec672bc4a632
Caleb Flanagan <caleb.flanagan@clearlink.com>
2019-12-03 10:39:08 -0700

    adds sloth name dataset
    Signed-off-by: Caleb Flanagan <caleb.flanagan@clearlink.com>

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,4 +1,4 @@
-const availablePickers = ["romanceNovelTitle","rvName","superstarDiva","wweSuperstar","pirateName"];
+const availablePickers = ["romanceNovelTitle","rvName","superstarDiva","wweSuperstar","pirateName","slothName"];
 
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const months = ["january","february","march","april","may","june","july","august","september","october","november","december"];
@@ -134,6 +134,11 @@ const showMyResults = (picker, selections) => {
 
         case "pirateName":
           answerString = `${answersArray[0]} ${answersArray[1]} ${answersArray[2]}`;
+          finalAnswer.textContent = answerString;
+        break;
+
+        case "slothName":
+          answerString = `${answersArray[0]} the  ${answersArray[1]} ${answersArray[2]}`;
           finalAnswer.textContent = answerString;
         break;
 
